c1e7b8e5f38b415fa104bd9aa1ce152f28fa5857
djteiji <djteiji@gmail.com>
2022-05-10 02:36:18 -0700

    generate HTML working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 const fs = require('fs');
 // const { ADDRGETNETWORKPARAMS } = require('dns');
 
@@ -194,9 +195,32 @@ return inquirer.prompt ([
 
 // ]
 
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
 
 managerQuestions()
     .then(addMembers)
+    .then(teamArr => {
+        return generatePage(teamArr);
+      })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+        })
+        .catch(err => {
+         console.log(err);
+        });
+    
 
 // addManager()
 //   .then(addEmployee)
